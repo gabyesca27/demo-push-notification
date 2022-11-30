1169f2fb35a0afcd9b3ca0aecfbe94d3d21b5102
Gabriela Noemi Escalante <gaby.noemi.esca@gmail.com>
2022-11-30 00:49:31 -0300

    modifications

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import addNotification from 'react-push-notification';
-import { Notifications } from 'react-push-notification';
+
+import { useEffect } from "react";
 function App() {
-/*function buttonOnClick (){
+  /*function buttonOnClick (){
 	addNotification({
 	title: 'Warning',
 	native:true		
 	})
 };*/
 
-function notifyMe() {
-  // Comprobamos si el navegador soporta las notificaciones
-  if (!("Notification" in window)) {
-    alert("Este navegador no es compatible con las notificaciones de escritorio");
+  function notifyMe() {
+    // Comprobamos si el navegador soporta las notificaciones
+    if (!("Notification" in window)) {
+      alert(
+        "Este navegador no es compatible con las notificaciones de escritorio"
+      );
+    }
+
+    // Comprobamos si los permisos han sido concedidos anteriormente
+    else if (Notification.permission === "granted") {
+      //    Si es correcto, lanzamos una notificación
+      new Notification("¡Hola!");
+    }
+
+    // Si no, pedimos permiso para la notificación
+    else if (Notification.permission !== "denied") {
+      console.log("Entro en este if");
+      Notification.requestPermission().then(function (permission) {
+        // Si el usuario nos lo concede, creamos la notificación
+        if (permission === "granted") {
+          new Notification("¡Hola!");
+        }
+      });
+    }
+
+    // Por último, si el usuario ha denegado el permiso,
+    // y quiere ser respetuoso, no hay necesidad de molestarlo.
   }
+  useEffect(() => {
+    notifyMe();
+  }, []);
 
-  // Comprobamos si los permisos han sido concedidos anteriormente
-  else if (Notification.permission === "granted") {
-    // Si es correcto, lanzamos una notificación
-    var notification = new Notification("¡Hola!");
-  }
-
-  // Si no, pedimos permiso para la notificación
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      // Si el usuario nos lo concede, creamos la notificación
-      if (permission === "granted") {
-        var notification = new Notification("¡Hola!");
-      }
-    });
-  }
-
-  // Por último, si el usuario ha denegado el permiso,
-  // y quiere ser respetuoso, no hay necesidad de molestarlo.
-}
-
-return (
-	<div className="App">
-	<Notifications />
-  <button onClick={notifyMe()}>¡Notifícame!</button>
-  </div>
-);
+  return (
+    <div className="App">
+      <button onClick={() => notifyMe()}>¡Notifícame!</button>
+    </div>
+  );
 }
 
 export default App;
-
