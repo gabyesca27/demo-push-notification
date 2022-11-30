dfa4916c870b1c819ff302f1e92acc971206d17b
Gabriela Noemi Escalante <gaby.noemi.esca@gmail.com>
2022-11-30 00:00:07 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import addNotification from 'react-push-notification';
+import { Notifications } from 'react-push-notification';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*function buttonOnClick (){
+	addNotification({
+	title: 'Warning',
+	native:true		
+	})
+};*/
+
+function notifyMe() {
+  // Comprobamos si el navegador soporta las notificaciones
+  if (!("Notification" in window)) {
+    alert("Este navegador no es compatible con las notificaciones de escritorio");
+  }
+
+  // Comprobamos si los permisos han sido concedidos anteriormente
+  else if (Notification.permission === "granted") {
+    // Si es correcto, lanzamos una notificación
+    var notification = new Notification("¡Hola!");
+  }
+
+  // Si no, pedimos permiso para la notificación
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // Si el usuario nos lo concede, creamos la notificación
+      if (permission === "granted") {
+        var notification = new Notification("¡Hola!");
+      }
+    });
+  }
+
+  // Por último, si el usuario ha denegado el permiso,
+  // y quiere ser respetuoso, no hay necesidad de molestarlo.
+}
+
+return (
+	<div className="App">
+	<Notifications />
+  <button onClick={notifyMe()}>¡Notifícame!</button>
+  </div>
+);
 }
 
 export default App;
+
